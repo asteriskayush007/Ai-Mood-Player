f307d6f9a929a4c5e93716de667067c211b4d1b7
asteriskayush007 <ayushmaansinghyadav@yahoo.com>
2025-07-21 22:43:46 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,7 @@ const app = require('./src/app')
 
 const connectDB = require('./src/db/db')
 
-app.get('./',(req,res)=>{
-    res.send({
-        activeStatus:true,
-        error:false,
-    })
-})
+
 
 connectDB();
 
--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,12 @@ const cors = require('cors');
 
 const app = express();
 
-
+app.get('./',(req,res)=>{
+    res.send({
+        activeStatus:true,
+        error:false,
+    })
+})
 
 
 app.use(express.json());
